2e606cdcd49ef03aec0741142526478da060debc
b6320502509 <sasicha.sal@ku.th>
2021-03-25 17:19:59 +0700

    2

--- 1.c
+++ 1.c
@@ -1,1 +1,30 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,a=2,i,j=0,k=0;
+    scanf("%d",&n);
+    for(i=a; i<=1000; i++)
+    {
+        if(n==(a*a*a))
+        {
+            j++;
+            break;
+        }
+    }
+    for(i=1; i<=n; i++)
+    {
+        if(i==(a*a*a))
+        {
+            k++;
+        }
+    }
+    if(j==0)
+    {
+        printf("%d",n-k);
+    }
+    else
+    {
+        printf("Not Cube Free");
+    }
+    return 0;
+}
