1a7e49e0c218b9bb1d6652ac1f1e57f2feee5f6e
b6320502509 <sasicha.sal@ku.th>
2021-03-25 18:05:30 +0700

    3

--- 2.c
+++ 2.c
@@ -1,1 +1,35 @@
-
+#include<stdio.h>
+int main()
+{
+    int m,n,i,j,p,q,r;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    char b[n][70];
+    for(i=0; i<n; i++)
+    {
+        scanf("%d %d %d",&p,&q,&r);
+        for(j=q; j<(r+q); j++)
+        {
+            b[p-1][j-1]='x';
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<70; j++)
+        {
+            if(b[i][j]!='x')
+            {
+                b[i][j]='o';
+            }
+        }
+    }
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<70; j++)
+        {
+            printf("%c",b[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
